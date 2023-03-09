f63f38d5a902cfa57d1872ef76d583832f3d812f
mojahid-belaman <mojahidbelaman@gmail.com>
2023-03-09 19:03:00 +0100

    style layout input container

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,9 +4,19 @@ import PrimaryButton from "../components/atoms/PrimaryButton";
 const StartGameScreen = () => {
   return (
     <View style={styles.boxContainer}>
-      <TextInput style={styles.inputContainer} />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <TextInput
+        style={styles.inputContainer}
+        maxLength={2}
+        keyboardType="numeric"
+      />
+      <View style={styles.boxButton}>
+        <View style={styles.btnContainer}>
+          <PrimaryButton onPress={() => {}}>Reset</PrimaryButton>
+        </View>
+        <View style={styles.btnContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 };
@@ -18,19 +28,27 @@ const styles = StyleSheet.create({
     marginTop: 80,
     marginHorizontal: 20,
     padding: 20,
-    backgroundColor: "#8b0000",
+    backgroundColor: "#4a148c",
     borderRadius: 8,
     elevation: 10,
+    alignItems: "center",
   },
   inputContainer: {
     height: 40,
     width: 80,
     paddingHorizontal: 10,
+    marginBottom: 20,
     borderBottomColor: "#ffd700",
-    color: '#ffd700',
-    borderWidth: 2,
+    color: "#ffd700",
+    borderBottomWidth: 2,
     fontSize: 30,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  boxButton: {
+    flexDirection: "row",
+  },
+  btnContainer: {
+    flex: 1
   }
 });
