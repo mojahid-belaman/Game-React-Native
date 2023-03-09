fc7f2f99ae3e586dfd5b6bff8b81ab51d9d48b0c
mojahid-belaman <mojahidbelaman@gmail.com>
2023-03-09 19:02:08 +0100

    add more style and Pressed Component

--- components/atoms/PrimaryButton.js
+++ components/atoms/PrimaryButton.js
@@ -1,11 +1,45 @@
-import { Text, View } from "react-native"
+import { Pressable, Text, View, StyleSheet } from "react-native";
 
-const PrimaryButton = ({children}) => {
-    return (
-        <View>
-            <Text>{children}</Text>
-        </View>
-    )
-}
+const PrimaryButton = ({ children }) => {
+  function pressHandler() {
+    console.log("Pressed!");
+  }
+  return (
+    <View style={styles.btnOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.btnInnerContainer, styles.pressed]
+            : styles.btnInnerContainer
+        }
+        onPress={pressHandler}
+        android_ripple={{ color: "#ab47bc" }}
+      >
+        <Text style={styles.btnText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
+};
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  btnOuterContainer: {
+    borderRadius: 30,
+    margin: 5,
+    overflow: "hidden",
+  },
+  btnInnerContainer: {
+    backgroundColor: "#6a1b9a",
+    paddingHorizontal: 6,
+    paddingVertical: 10,
+    elevation: 5,
+  },
+  btnText: {
+    color: "#FFFFFF",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
