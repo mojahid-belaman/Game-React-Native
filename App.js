4cc060f40254c1289d2e4ca8e71c6d3038a87747
mojahid-belaman <mojahidbelaman@gmail.com>
2023-03-11 09:49:53 +0100

    navigate with screens without dependence

--- App.js
+++ App.js
@@ -1,17 +1,40 @@
-import { StyleSheet, View } from "react-native";
+import { ImageBackground, StyleSheet } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
+import { LinearGradient } from "expo-linear-gradient";
+import { useState } from "react";
+import GameScreen from "./screens/GameScreen";
 
 export default function App() {
+  const [pickNumber, setPickNumber] = useState();
+
+  function pickNumberHandler(pickNumber) {
+    setPickNumber(pickNumber);
+  }
+
+  let screen = <StartGameScreen onScreenHandler={pickNumberHandler} />;
+
+  if (pickNumber) {
+    screen = <GameScreen />;
+  }
   return (
-    <View style={styles.rootScreen}>
-      <StartGameScreen />
-    </View>
+    <LinearGradient colors={["#ba68c8", "#4a148c"]} style={styles.rootScreen}>
+      <ImageBackground
+        source={require("./assets/images/dice.jpg")}
+        resizeMode={"cover"}
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        {screen}
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
-    backgroundColor: "#b39ddb",
-  }
+  },
+  backgroundImage: {
+    opacity: 0.15,
+  },
 });
