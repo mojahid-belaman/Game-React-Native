3cfb0818035c3b945d03703654da42049ff9da35
mojahid-belaman <mojahidbelaman@gmail.com>
2023-03-11 17:57:06 +0100

    create function generate random number

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,35 @@
-import { Text } from "react-native";
+import { useState } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import GuessNumber from "../components/atoms/GuessNumber";
+import Title from "../components/atoms/Title";
 
-const GameScreen = () => {
-  return <Text>Game Screen!</Text>;
+function generateNumberRandom(min, max, exclud) {
+  const randomNumber = Math.floor(Math.random() * (max - min)) + min
+
+  if (randomNumber === exclud)
+    return generateNumberRandom(min, max, exclud)
+  else
+    return randomNumber
+}
+
+const GameScreen = ({inputNumber}) => {
+  const initialGuessNumber = generateNumberRandom(1, 100, inputNumber)
+  const [guessNumber, setGuessNumber] = useState(initialGuessNumber)
+  return (
+    <View style={styles.screen}>
+      <Title>Opponent's Guess</Title>
+      <GuessNumber>{guessNumber}</GuessNumber>
+      <View>
+        <Text>Higher or Lower ?</Text>
+      </View>
+    </View>
+  );
 };
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 24,
+  },
+});
