be62e6ccbbcaf4e1f0cf8ea18de7c1541365f76a
mojahid-belaman <mojahidbelaman@gmail.com>
2023-03-11 17:58:56 +0100

    create custom component GuessNumber

---
+++ components/atoms/GuessNumber.js
@@ -0,0 +1,29 @@
+import { Text, View, StyleSheet } from "react-native";
+import colors from "../../constants/colors";
+
+const GuessNumber = ({ children }) => {
+  return (
+    <View style={styles.boxContainer}>
+      <Text style={styles.numberText}>{children}</Text>
+    </View>
+  );
+};
+
+export default GuessNumber;
+
+const styles = StyleSheet.create({
+  boxContainer: {
+    padding: 20,
+    margin: 30,
+    borderWidth: 2,
+    backgroundColor: colors.purple900,
+    borderColor: colors.purple100,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  numberText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: colors.yellow700,
+  }
+});
