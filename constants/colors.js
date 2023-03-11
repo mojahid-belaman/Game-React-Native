d8316b028c4253dcb068840f2b36ea704046345b
mojahid-belaman <mojahidbelaman@gmail.com>
2023-03-11 17:55:21 +0100

    define all colors to need

---
+++ constants/colors.js
@@ -0,0 +1,11 @@
+const colors = {
+    purple900: '#4a148c',
+    purple800: '#6a1b9a',
+    purple400: '#ab47bc',
+    purple300: '#ba68c8',
+    purple100: '#e1bee7',
+    yellow700: '#fbc02d',
+    white: '#FFFFFF'
+}
+
+export default colors;
