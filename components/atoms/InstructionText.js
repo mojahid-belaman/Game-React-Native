de871ab9515c75496900cb326682e8cc36419e94
mojahid-belaman <mojahidbelaman@gmail.com>
2023-03-15 16:35:46 +0100

    merge default style with new style

--- components/atoms/InstructionText.js
+++ components/atoms/InstructionText.js
@@ -1,8 +1,8 @@
 import { Text, StyleSheet } from "react-native";
 import Colors from "../../constants/colors";
 
-const InstructionText = ({ children }) => {
-  return <Text style={styles.instructionText}>{children}</Text>;
+const InstructionText = ({ children, style }) => {
+  return <Text style={[styles.instructionText, style]}>{children}</Text>;
 };
 
 export default InstructionText;
@@ -12,6 +12,5 @@ const styles = StyleSheet.create({
     color: Colors.yellow700,
     fontSize: 20,
     opacity: 0.75,
-    marginBottom: 15,
   },
 });
